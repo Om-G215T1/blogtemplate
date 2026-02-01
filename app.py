09c5e355f028bd37026462aed01039efbae6710a
Om-G215T1 <omg151315@gmail.com>
2026-02-01 18:48:39 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('blog.html')
+
+@app.route('/post/<int:id>')
+def post_detail(id):
+    return render_template('single.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
